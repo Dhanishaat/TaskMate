ef83e0865ce9f8a08f040c75b06fda93a61df06d
Dhanishaat <dhanishamannur@gmail.com>
2025-11-30 21:34:21 +0530

    30th nov committ

--- src/Components/ProgressTracker.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function ProgressTracker() {
-  return (
-    <div>
-      <h1>Progress tracker component</h1>
-    </div>
-  )
-}
--- src/Components/TaskList.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function TaskList() {
-  return (
-    <div>
-      <h1>Tasklist component</h1>
-    </div>
-  )
-}
